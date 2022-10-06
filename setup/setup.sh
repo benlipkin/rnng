e3a97f9407ad2fee9930488c070b92765c2013fd
benlipkin <bglipkin@gmail.com>
2022-10-06 14:39:24 -0400

    fixed bug in setup; needed to mkdir models

--- setup/setup.sh
+++ setup/setup.sh
@@ -2,17 +2,14 @@
 
 set -e
 
+conda install -yc anaconda wget
+
 HOME_DIR="$(dirname $(pwd))"
-
-wget=$(conda list wget | grep -c wget)
-if [ "$wget" -eq 0 ]; then
-    conda install -yc anaconda wget
-fi
-
 FILEID="$1"
 FNAME="$HOME_DIR/models/$FILEID"
 
 BASE="https://huggingface.co/datasets/benlipkin/rnng-brainscore/resolve/main"
 if [ ! -f "$FNAME" ]; then
+    mkdir -p "$(dirname $FNAME)"
     wget -O "$FNAME" "$BASE/$FILEID"
 fi
